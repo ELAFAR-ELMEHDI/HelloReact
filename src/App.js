7736be73f689eceee09affe74f33510f7162d27d
EL AFAR EL MEHDI <54406762+ELAFAR-ELMEHDI@users.noreply.github.com>
2019-12-05 10:38:55 +0100

    state
    simple state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React,{Component} from "react";
-
-
+import React, { Component } from "react";
 
 class App extends Component {
-  render(){
-  return (
-    <div className="App">
-      <h1>Hello from {this.props.name}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from {this.props.name}</h1>
+        <h2>mon compteur {this.state.count}</h2>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import "./styles.css";
 
-
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App name="mehdi"/>, rootElement);
+ReactDOM.render(<App name="mehdi" />, rootElement);
