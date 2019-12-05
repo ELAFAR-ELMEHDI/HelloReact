a1eb5528b6da70515a4b9c055c1992099e87a373
EL AFAR EL MEHDI <54406762+ELAFAR-ELMEHDI@users.noreply.github.com>
2019-12-05 10:53:18 +0100

    set state
    set state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ class App extends Component {
       count: 0
     };
   }
+  set() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello from {this.props.name}</h1>
         <h2>mon compteur {this.state.count}</h2>
+        <button onClick={() => this.set()}>Ajouter</button>
       </div>
     );
   }
